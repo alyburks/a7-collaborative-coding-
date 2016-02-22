d81b98d4af21c55bb42a8d9d834da29138c305cf
Alyxis Burks <alyxistarynburks@gmail.com>
2016-02-22 09:34:58 -0800

    Now it's still weird

--- visualization1.R
+++ visualization1.R
@@ -4,11 +4,13 @@ library(dplyr)
 
 data <- read.csv("https://raw.githubusercontent.com/INFO-498F/a7-survey-data/master/intro_survey_data.csv")
 data_info <- filter(data, What.is.your.current.class.standing. == 'Sophomore')
+students <- nrow(data)
 #Creats bar graph based on how much prior coding experience people had before the class
 visualization1 <- function(data){
   #Detail about bar graph
   p <- plot_ly(data,
         x = Are.you.interested.in.applying.to.the.Informatics.major. ,
+        y = students ,
         type = "bar", 
         marker = list(color = toRGB("blue3"))) %>% 
       #Layouts the bar graph
