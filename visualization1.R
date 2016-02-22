80bbbb65e17717bab780b4f9ff0488845f9b2637
Alyxis Burks <alyxistarynburks@gmail.com>
2016-02-21 21:18:14 -0800

    Uhhhhh what

--- visualization1.R
+++ visualization1.R
@@ -1,16 +1,22 @@
 #Creates visualization 1 
 library(plotly)
+library(dplyr)
 
-
+data <- read.csv("https://raw.githubusercontent.com/INFO-498F/a7-survey-data/master/intro_survey_data.csv")
+data_info <- filter(data, What.is.your.current.class.standing. == 'Sophomore')
 #Creats bar graph based on how much prior coding experience people had before the class
 visualization1 <- function(data){
   #Detail about bar graph
   p <- plot_ly(data,
-        x = `What is your programming experience?` ,
-        y = `Amount of Students`, 
+        x = Are.you.interested.in.applying.to.the.Informatics.major. ,
         type = "bar", 
         marker = list(color = toRGB("blue3"))) %>% 
       #Layouts the bar graph
-      layout(title = 'Coding before INFO498') 
-  return(p)
+      layout(title = 'Coding before INFO498')
+ return(p)
 }
+  
+  
+visualization1(data)
+
+
