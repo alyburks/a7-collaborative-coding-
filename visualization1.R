b470ecbc9223856d6ef12f0917495aea369e52bb
Alyxis Burks <alyxistarynburks@gmail.com>
2016-02-21 20:36:26 -0800

    uh

--- visualization1.R
+++ visualization1.R
@@ -1,12 +1,16 @@
 #Creates visualization 1 
 library(plotly)
 
+
+#Creats bar graph based on how much prior coding experience people had before the class
 visualization1 <- function(data){
+  #Detail about bar graph
   p <- plot_ly(data,
         x = `What is your programming experience?` ,
         y = `Amount of Students`, 
         type = "bar", 
         marker = list(color = toRGB("blue3"))) %>% 
-      layout(title = 'Coding before INFO498',) 
+      #Layouts the bar graph
+      layout(title = 'Coding before INFO498') 
   return(p)
 }
