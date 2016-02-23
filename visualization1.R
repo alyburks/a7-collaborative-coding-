fb9e7f564a85fb6911796f215a9c58ffa0739ad8
Alyxis Burks <alyxistarynburks@gmail.com>
2016-02-22 23:53:19 -0800

    Fixed Variable name

--- visualization1.R
+++ visualization1.R
@@ -3,7 +3,7 @@ data_info_for_v1 <- table(dataInfo$Are.you.interested.in.applying.to.the.Informa
 #Creats bar graph based on how much prior coding experience people had before the class
 visualization1 <- function(data){
   #Detail about bar graph
-  p <- barplot(data_info,
+  p <- barplot(data_info_for_v1,
         xlab = "Interested in Applying to the Informatics Major" ,
         main = 'Appling to informatics')
  return(p)
