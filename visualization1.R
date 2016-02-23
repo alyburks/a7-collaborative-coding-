4e9de8ff7b8f9af1d404d0f64a18009818b4bf7b
Evan Taylor <evantobytaylor@gmail.com>
2016-02-23 00:06:36 -0800

    index.Rmd and more

--- visualization1.R
+++ visualization1.R
@@ -1,14 +1,11 @@
-data_info_for_v1 <- table(dataInfo$Are.you.interested.in.applying.to.the.Informatics.major.)
-
 #Creats bar graph based on how much prior coding experience people had before the class
 visualization1 <- function(data){
   #Detail about bar graph
   p <- barplot(data_info_for_v1,
         xlab = "Interested in Applying to the Informatics Major" ,
+        ylab = "Total Responses",
+        col = ("blue"),
         main = 'Appling to informatics')
  return(p)
 }
   
-
-
-
