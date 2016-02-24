23d25e4912f1f36b17b7589ae55623c09c0ca07f
Alyxis Burks <alyxistarynburks@gmail.com>
2016-02-23 16:09:05 -0800

    Needs second visualization

--- visualization1.R
+++ visualization1.R
@@ -1,11 +1,11 @@
 #Creats bar graph based on how much prior coding experience people had before the class
 visualization1 <- function(data){
   #Detail about bar graph
-  p <- barplot(data_info_for_v1,
+  p <- barplot(data,
         xlab = "Interested in Applying to the Informatics Major" ,
         ylab = "Total Responses",
         col = ("blue"),
-        main = 'Appling to informatics')
+        main = 'Applying to informatics')
  return(p)
 }
   
